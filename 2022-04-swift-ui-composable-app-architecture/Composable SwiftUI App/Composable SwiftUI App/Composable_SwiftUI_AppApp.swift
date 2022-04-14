435c73f028649aff3239b70fd33bdcff6bc9b88d
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2022-04-14 16:54:20 +1000

    Project init: Composable SwiftUI App

---
+++ 2022-04-swift-ui-composable-app-architecture/Composable SwiftUI App/Composable SwiftUI App/Composable_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Composable_SwiftUI_AppApp.swift
+//  Composable SwiftUI App
+//
+//  Created by Nicholas Tian on 14/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct Composable_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
