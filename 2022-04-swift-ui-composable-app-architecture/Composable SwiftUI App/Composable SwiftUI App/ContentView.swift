1f01d29ea3bf57cb4df6885419dcad118286ca06
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2022-04-24 15:40:53 +1000

    CSA: nthPrime working with WolframAlpha!!!

--- 2022-04-swift-ui-composable-app-architecture/Composable SwiftUI App/Composable SwiftUI App/ContentView.swift
+++ 2022-04-swift-ui-composable-app-architecture/Composable SwiftUI App/Composable SwiftUI App/ContentView.swift
@@ -7,6 +7,64 @@
 
 import SwiftUI
 
+struct WolframAlphaResult: Decodable {
+    let queryresult: QueryResult
+    
+    struct QueryResult: Decodable {
+        let pods: [Pod]
+        
+        struct Pod: Decodable {
+            let primary: Bool?
+            let subpods: [SubPod]
+            
+            struct SubPod: Decodable {
+                let plaintext: String
+            }
+        }
+    }
+}
+
+func nthPrime(_ n: Int, callback: @escaping (Int?) -> Void) {
+    wolframAlpha(query: "prime \(n)") { result in
+        callback(
+            result
+                .flatMap {
+                    $0.queryresult
+                        .pods
+                        .first { $0.primary == true }?
+                        .subpods
+                        .first?
+                        .plaintext
+                }
+                .flatMap(Int.init)
+        )
+    }
+}
+
+func wolframAlpha(
+    query: String,
+    callback: @escaping (WolframAlphaResult?) -> Void
+) {
+    var components = URLComponents(string: "https://api.wolframalpha.com/v2/query")!
+    
+    components.queryItems = [
+        URLQueryItem(name: "input", value: query),
+        URLQueryItem(name: "format", value: "plaintext"),
+        URLQueryItem(name: "output", value: "JSON"),
+        URLQueryItem(name: "appid", value: Keychain.wolframAlphaAPIKey)
+    ]
+    
+    let task = URLSession.shared.dataTask(with: components.url(relativeTo: nil)!) { data, response, error in
+        let result = data.flatMap {
+            try? JSONDecoder().decode(WolframAlphaResult.self, from: $0)
+        }
+        
+        callback(result)
+    }
+    
+    task.resume()
+}
+
 struct ContentView: View {
     @StateObject var state = AppState()
     
@@ -139,7 +197,9 @@ struct CounterView: View {
                 Text("Is this prime?")
             }
             Button {
-
+                nthPrime(state.count) { optionalPrime in
+                    print(optionalPrime)
+                }
             } label: {
                 Text("What is the \(ordinal(state.count)) prime?")
             }
---
+++ 2022-04-swift-ui-composable-app-architecture/Composable SwiftUI App/Composable SwiftUI App/Keychain.example.swift
@@ -0,0 +1,17 @@
+//
+//  Keychain.swift
+//  Composable SwiftUI App
+//
+//  Created by Nicholas The Personal on 24/4/2022.
+//
+
+import Foundation
+
+// 1. Copy the following
+// 2. Create a new file called `Keychain.swift` in the CSA target
+// 3. Paste the copied content
+// 4. Change the API key to your own
+
+enum Keychain {
+    static let wolframAlphaAPIKey = "YOUR-API-KEY-HERE"
+}
