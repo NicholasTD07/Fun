e3c370b5c41192225899f7246036eba842888a9a
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2022-05-12 15:23:42 +1000

    CSA: pullback for reducers

--- 2022-04-swift-ui-composable-app-architecture/Composable SwiftUI App/Composable SwiftUI App/AppState.swift
+++ 2022-04-swift-ui-composable-app-architecture/Composable SwiftUI App/Composable SwiftUI App/AppState.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import FileProvider
 
 /*
  
@@ -18,7 +19,7 @@ import Foundation
  */
 
 final class Store<Value, Action>: ObservableObject {
-    @Published var value: Value
+    @Published private(set) var value: Value
     private let reducer: (inout Value, Action) -> Void
     
     init(initialValue: Value, reducer:  @escaping (inout Value, Action) -> Void) {
@@ -31,12 +32,164 @@ final class Store<Value, Action>: ObservableObject {
     }
 }
 
+func combine<Value, Action>(
+  _ first: @escaping (inout Value, Action) -> Void,
+  _ second: @escaping (inout Value, Action) -> Void
+) -> (inout Value, Action) -> Void {
+    { value, action in
+        first(&value, action)
+        second(&value, action)
+    }
+}
+
+func combine<Value, Action>(
+  reducers: [(inout Value, Action) -> Void]
+) -> (inout Value, Action) -> Void {
+    { value, action in
+        reducers.forEach { reducer in
+            reducer(&value, action)
+        }
+    }
+}
+
+func pullback<LocalValue, GlobalValue, Action> (
+    reducer: @escaping (inout LocalValue, Action) -> Void,
+    get: @escaping (GlobalValue) -> LocalValue,
+    set: @escaping (inout GlobalValue, LocalValue) -> Void
+) -> (inout GlobalValue, Action) -> Void {
+    { globalValue, action in
+        var localValue = get(globalValue)
+        
+        reducer(&localValue, action)
+        
+        set(&globalValue, localValue)
+    }
+}
+
+// pullback but with immutable reducers
+//func pullback<LocalValue, GlobalValue, Action> (
+//    reducer: @escaping (LocalValue, Action) -> LocalValue,
+//    get: @escaping (GlobalValue) -> LocalValue,
+//    set: @escaping (inout GlobalValue, LocalValue) -> Void
+//) -> (inout GlobalValue, Action) -> Void {
+//    { globalValue, action in
+//        let localValue = get(globalValue)
+//
+//        let updated = reducer(localValue, action)
+//
+//        set(&globalValue, updated)
+//    }
+//}
+
+func counterReducer(value: inout Int, action: AppAction) {
+    switch action {
+    case .counter(let counterAction):
+        switch counterAction {
+        case .decrTapped:
+            return value -= 1
+        case .incrTapped:
+            return value += 1
+        }
+    case .primeModal:
+        break
+    case .favoritePrimes:
+        break
+    }
+}
+
+
+func pullback<LocalValue, GlobalValue, Action> (
+    reducer: @escaping (inout LocalValue, Action) -> Void,
+    keyPath: WritableKeyPath<GlobalValue, LocalValue>
+) -> (inout GlobalValue, Action) -> Void {
+    { (globalValue, action) in
+        reducer(&globalValue[keyPath: keyPath], action)
+    }
+}
+
+func test() {
+    let reducer = pullback(
+        reducer: counterReducer(value:action:),
+        get: { (state: AppState) in state.count },
+        set: { (state: inout AppState, value: Int) in state.count = value }
+    )
+    
+    let keyPath = \AppState.count
+    let states: [AppState] = []
+    let mappedCount = states.map(\.count)
+    
+    let anotherReducer = pullback(
+        reducer: counterReducer(value:action:),
+        keyPath: keyPath
+    )
+    
+    let combined = combine(reducers: [reducer])
+}
+
+struct FavoritePrimesState {
+    var favoritePrimes: [Int]
+    var activityFeed: [AppState.Activity]
+}
+
+func favoritePrimesReducer(value: inout FavoritePrimesState, action: AppAction) {
+    switch action {
+    case .counter(let counterAction):
+        break
+    case .primeModal:
+        break
+    case .favoritePrimes(let favoritePrimeAction):
+        switch favoritePrimeAction {
+        case .deleteFavoritePrimes(let indexSet):
+            
+            for index in indexSet {
+                let activity = AppState.Activity(
+                    timestamp: Date(),
+                    type: .removedFavoritePrime(
+                        value.favoritePrimes[index]
+                    )
+                )
+
+                value.activityFeed.append(activity)
+                value.favoritePrimes.remove(at: index)
+            }
+        }
+    }
+}
+
+func moreTest() {
+    let reducers = [
+        pullback(
+            reducer: counterReducer(value:action:),
+            keyPath: \AppState.count
+        ),
+        pullback(
+            reducer: favoritePrimesReducer(value:action:),
+            keyPath: \AppState.favoritePrimesState
+        )
+    ]
+    
+    let combined = combine(reducers: reducers)
+}
+
 struct AppState {
     var count = 0
     var savedPrimes: [Int] = []
     var loggedInUser: User? = nil
     var activityFeed: [Activity] = []
     
+    var favoritePrimesState: FavoritePrimesState {
+        get {
+            return FavoritePrimesState(
+                favoritePrimes: savedPrimes,
+                activityFeed: activityFeed
+            )
+        }
+        set {
+            savedPrimes = newValue.activityFeed
+            activityFeed = newValue.activityFeed
+        }
+    }
+        
     struct Activity {
         let timestamp: Date
         let type: ActivityType
