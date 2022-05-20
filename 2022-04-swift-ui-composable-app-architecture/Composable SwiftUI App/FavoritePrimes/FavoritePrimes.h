a3d0f2da08b48b282070a68226e4afc0bf3e31c4
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2022-05-20 18:29:19 +1000

    CSA: Prep for modularizing

---
+++ 2022-04-swift-ui-composable-app-architecture/Composable SwiftUI App/FavoritePrimes/FavoritePrimes.h
@@ -0,0 +1,18 @@
+//
+//  FavoritePrimes.h
+//  FavoritePrimes
+//
+//  Created by Nicholas The Personal on 20/5/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FavoritePrimes.
+FOUNDATION_EXPORT double FavoritePrimesVersionNumber;
+
+//! Project version string for FavoritePrimes.
+FOUNDATION_EXPORT const unsigned char FavoritePrimesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FavoritePrimes/PublicHeader.h>
+
+
